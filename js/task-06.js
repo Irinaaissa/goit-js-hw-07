ab71d67b5d48475eea6bbd87dc78d140b3375dba
Irinaaissa <irinaalisa7506@gmail.com>
2023-12-01 19:31:08 +0200

    1

---
+++ js/task-06.js
@@ -0,0 +1,36 @@
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const controlsDiv = document.querySelector("#controls");
+  const boxesDiv = document.querySelector("#boxes");
+  const createButton = controlsDiv.querySelector('[data-create]');
+  const destroyButton = controlsDiv.querySelector('[data-destroy]');
+  const input = controlsDiv.querySelector('input');
+
+  createButton.addEventListener('click', createBoxes);
+  destroyButton.addEventListener('click', destroyBoxes);
+
+  function createBoxes() {
+    const amount = Number(input.value);
+    if (amount >= 1 && amount <= 100) {
+      const boxes = [];
+      for (let i = 0; i < amount; i++) {
+        const box = document.createElement('div');
+        const size = 30 + i * 10;
+        const color = getRandomHexColor();
+        box.style.width = `${size}px`;
+        box.style.height = `${size}px`;
+        box.style.backgroundColor = color;
+        boxes.push(box);
+      }
+      boxesDiv.append(...boxes);
+    }
+    input.value = '';
+  }
+
+  function destroyBoxes() {
+    boxesDiv.innerHTML = '';
+  }
